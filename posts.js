a2f91f5c1f38c26ea26e695cc8e529e65932fe95
UwUWare <125191125+Ir0xWare@users.noreply.github.com>
2025-06-03 20:14:57 +0200

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,12 @@
+const posts = [
+  {
+    title: "Welcome to the irox Hub",
+    date: "2025-06-03",
+    content: "This is my little personal forum/blog where I just post what I’m thinking about."
+  },
+  {
+    title: "Making gieno.html",
+    date: "2025-06-02",
+    content: "Working on a wiki about myself. It’s fun documenting everything like a project log."
+  }
+];
