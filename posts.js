16e5c791a9aee9fddac132150db0eb73240e4126
UwUWare <125191125+Ir0xWare@users.noreply.github.com>
2025-07-26 14:08:09 +0200

    Update posts.js

--- posts.js
+++ posts.js
@@ -3,5 +3,10 @@ const posts = [
     title: "fuck the eu.",
     date: "09.06.2025",
     content: "honestly, fuck the eu. They want to ban my freedom on the internet. The only thing that keeps me sane and somewhat stable: janitorai. I despise this stupid ass alliance. im legit thinking about murdering someone. i am not doing well. wtf eu. fuck eu. FUCK ALL OF THEM."
+  },
+  {
+    title: "that aged well lol",
+    date: "26.07.2025",
+    content: "well jai is still up and im fine lmao."
   }
 ];
